2327ffca6836fc1dea501092e30f3e7cf1091d26
Mark Murphy <mmurphy@commonsware.com>
2010-10-22 13:50:46 -0400

    Copyright header fixes

--- Rotation/RotationOne/src/com/commonsware/android/rotation/one/RotationOneDemo.java
+++ Rotation/RotationOne/src/com/commonsware/android/rotation/one/RotationOneDemo.java
@@ -1,16 +1,15 @@
-/* Copyright (c) 2008-2010 -- CommonsWare, LLC
-
-	 Licensed under the Apache License, Version 2.0 (the "License");
-	 you may not use this file except in compliance with the License.
-	 You may obtain a copy of the License at
-
-		 http://www.apache.org/licenses/LICENSE-2.0
-
-	 Unless required by applicable law or agreed to in writing, software
-	 distributed under the License is distributed on an "AS IS" BASIS,
-	 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	 See the License for the specific language governing permissions and
-	 limitations under the License.
+/***
+	Copyright (c) 2008-2010 CommonsWare, LLC
+	
+	Licensed under the Apache License, Version 2.0 (the "License"); you may
+	not use this file except in compliance with the License. You may obtain
+	a copy of the License at
+		http://www.apache.org/licenses/LICENSE-2.0
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
 */
 	 
 package com.commonsware.android.rotation.one;
@@ -34,25 +33,7 @@ public class RotationOneDemo extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		
-		Button btn=(Button)findViewById(R.id.pick);
-		
-		btn.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View view) {
-				Intent i=new Intent(Intent.ACTION_PICK,
-														Contacts.CONTENT_URI);
-
-				startActivityForResult(i, PICK_REQUEST);
-			}
-		});
-		
 		viewButton=(Button)findViewById(R.id.view);
-		
-		viewButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View view) {
-				startActivity(new Intent(Intent.ACTION_VIEW, contact));
-			}
-		});
-		
 		restoreMe(savedInstanceState);
 		
 		viewButton.setEnabled(contact!=null);
@@ -67,6 +48,17 @@ public class RotationOneDemo extends Activity {
 				viewButton.setEnabled(true);
 			}
 		}
+	}
+
+	public void pickContact(View v) {
+		Intent i=new Intent(Intent.ACTION_PICK,
+												Contacts.CONTENT_URI);
+	
+		startActivityForResult(i, PICK_REQUEST);
+	}
+	
+	public void viewContact(View v) {
+		startActivity(new Intent(Intent.ACTION_VIEW, contact));
 	}
 
 	@Override
