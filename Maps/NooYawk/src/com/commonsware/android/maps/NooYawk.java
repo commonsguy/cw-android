0555ba223aa70fe51860051062c80007d3e716da
Mark Murphy <mmurphy@commonsware.com>
2011-01-05 17:39:33 -0500

    Copyright year

--- Maps/NooYawk/src/com/commonsware/android/maps/NooYawk.java
+++ Maps/NooYawk/src/com/commonsware/android/maps/NooYawk.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2008-2010 -- CommonsWare, LLC
+/* Copyright (c) 2008-2011 -- CommonsWare, LLC
 
 	 Licensed under the Apache License, Version 2.0 (the "License");
 	 you may not use this file except in compliance with the License.
